82606d99a62db7e57354bb7e7119de924b8aa3e5
Pohao <pohao121@gmail.com>
2025-04-09 22:47:47 +0800

    feat(product): 設定產品資訊為輸入型屬性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,15 @@ import { ProductCartComponent } from './product-cart/product-cart.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'shopping-cart';
+  productName = '書籍A';
+  author = '作者甲、作者乙、作者丙';
+  company = '博碩文化';
+
+  isShow = true;
+
+  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+
+  onSetDisplay(isShow: boolean): void {
+    this.isShow = isShow;
+  }
 }
