89f5b5e50b5b7c1b5ec8ba1c06a3a9c83e33dc56
Pohao <pohao121@gmail.com>
2025-04-28 15:50:35 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { ProductCartComponent } from '../product-cart/product-cart.component';
 import { Product } from '../models/product';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-card-list',
-  imports: [ProductCartComponent],
+  imports: [PaginationComponent, ProductCartComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
   @Input()
   products!: Product[];
+
+  pageIndex = 1;
 }
