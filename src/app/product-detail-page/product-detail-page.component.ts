43239b70a532ac784a3c683cd88971c8d7554fd7
Pohao <pohao121@gmail.com>
2025-05-29 13:15:35 +0800

    feat(product-detail): 分頁刪除功能實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,8 +1,11 @@
 import { ProductService } from './../services/product.service';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, signal } from '@angular/core';
 import { Product } from '../models/product';
 import { CurrencyPipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { rxResource, takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -15,15 +18,50 @@ export class ProductDetailPageComponent {
 
   readonly router = inject(Router);
 
-  private productService = inject(ProductService);
+  private ProductService = inject(ProductService);
+
+  private destroyRef = inject(DestroyRef);
+
+  readonly searchControl = new FormControl<string | undefined>(undefined, { nonNullable: true });
+
+  readonly productName = toSignal(
+    this.searchControl.valueChanges.pipe(debounceTime(500), distinctUntilChanged(), takeUntilDestroyed(this.destroyRef)),
+    {
+      initialValue: undefined,
+    }
+  );
+
+  readonly pageIndex = signal(1);
+
+  readonly pageSize = signal(5);
+
+  private readonly data = rxResource({
+    request: () => ({ name: this.productName(), pageIndex: this.pageIndex(), pageSize: this.pageSize() }),
+    defaultValue: { data: [], count: 0 },
+    loader: ({ request }) => {
+      const { name, pageIndex, pageSize } = request;
+      return this.ProductService.getList(name, pageIndex, pageSize);
+    },
+  });
+
+  readonly totalCount = computed(() => {
+    const { count } = this.data.value();
+    return count;
+  });
+
+  readonly products = computed(() => {
+    const { data } = this.data.value();
+    return data;
+  });
 
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product().id]);
   }
 
-  onRemove(): void {
-    this.productService.remove(this.product().id);
-    this.router.navigate(['products']);
+  onRemove(p: Product): void {
+    this.ProductService.remove(p.id).subscribe(() => {
+      this.router.navigate(['products']);
+    });
   }
 
   onBack(): void {
